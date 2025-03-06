03b202a9a36d7666cb0f24e8f28b9ed3da2f8b54
111ash <dhawanashmita01@gmail.com>
2025-03-06 11:30:09 +0530

    Playing_with_characters.c

---
+++ Playing_with_characters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char a[100],b[100],c[100];
+    gets(a);
+    gets(b);
+    gets(c);
+    puts(a);
+    puts(b);
+    puts(c);   
+    return 0;
+}
